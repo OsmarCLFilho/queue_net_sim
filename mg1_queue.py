e647996b40f506956807d3ec9ed6e2215f49c84a
Osmar Cardoso Lopes Filho <osmarclopesfilho@gmail.com>
2024-12-09 15:52:46 -0300

    Update mg1 queue generating file

--- mg1_queue.py
+++ mg1_queue.py
@@ -7,14 +7,33 @@ rng = np.random.default_rng()
 
 queue_net = Network()
 
-# queue_id = queue_net.add_queue(fnt.partial(rng.uniform, low=2, high=4))
-queue_id = queue_net.add_queue(lambda: abs(rng.normal(loc=0, scale=1)))
-queue_net.add_entry_point(fnt.partial(rng.exponential, scale=1), queue_id)
+queue_id = queue_net.add_queue(fnt.partial(rng.uniform, low=2, high=4))
+# queue_id = queue_net.add_queue(lambda: abs(rng.normal(loc=0, scale=1)))
+queue_net.add_entry_point(fnt.partial(rng.exponential, scale=4), queue_id)
 
-(queues_history, timestamps) = queue_net.simulate(10)
+(queues_history, timestamps) = queue_net.simulate(10000)
 
+print("Clients in queue statistics:")
+x = [i[0] for i in queues_history]
+print("Mean: ", np.mean(x))
+print("Variance: ", np.var(x))
+
+from collections import deque
+client_count = 0
+starting_times = deque([])
+waiting_times = []
 for i in range(len(queues_history)):
-    print(timestamps[i], queues_history[i])
+    if queues_history[i][0] > client_count:
+        starting_times.append(timestamps[i])
+
+    if queues_history[i][0] < client_count:
+        st = starting_times.popleft()
+        waiting_times.append(timestamps[i] - st)
+
+    client_count = queues_history[i][0]
+
+print("Waiting time statistics:")
+print("Mean: ", np.mean(waiting_times))
 
 fig, ax = plt.subplots()
 
